6cf8f4761ae5c8097cd9ee20ff6f718baf843cd1
Fausto <faustocamp545@gmail.com>
2022-01-14 21:47:35 -0500

    Primer avance

---
+++ app/src/main/java/com/faustoc/zooapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.faustoc.zooapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ingresar (View view){
+
+        //Objeto para enlazarme al segundo activity
+        Intent intent = new Intent(this, SegundoActivity.class);
+        //Iniciar el activity
+        startActivity(intent);
+
+
+    }
+}
